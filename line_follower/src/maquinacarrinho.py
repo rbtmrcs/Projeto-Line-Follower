fe0762054337250a950feb5d62bf8cf76c519f75
Thiago <thiagomoutinho19@gmail.com>
2020-01-16 21:09:04 -0200

    maquina de estados faltando 2 estados funcionando

--- line_follower/src/maquinacarrinho.py
+++ line_follower/src/maquinacarrinho.py
@@ -14,17 +14,11 @@ class Sinal(smach.State):
         self.viu = False
 
     def callback(self, data):
-        rospy.loginfo("Entrou no callback")
-        rospy.loginfo("DADOS >{}<".format(data))
-        rospy.loginfo(type(data))
-
         if data == String("vai"):
-            rospy.loginfo("!!!!!!!!!!")
             self.viu = True
 
     def execute(self,userdata):
         rospy.sleep(2)
-        rospy.loginfo(self.viu)
         if self.viu:
             return 'foi'
         else:
@@ -42,32 +36,39 @@ class Andarobj(smach.State):
 
     def callback(self,data):
         print (data)
-        if data == 'bate la':
+        if data == String('bate la'):
             self.achou = True
 
     def callback1(self,data):
         self.informacao = data
 
     def execute(self, ud):
+        rospy.sleep(2)
         if self.achou:
             return 'foi'
 
         if self.informacao is None:
+            rospy.loginfo('??????')
             return 'repete'
 
-        if self.informacao == 'frente':
+        if self.informacao == String('frente'):
+            rospy.loginfo('!!!!!')
             self.controle.pra_frente()
 
-        elif self.informacao == 'direita':
+        elif self.informacao == String('direita'):
+            rospy.loginfo('!!!!!')
             self.controle.direita_light()
 
-        elif self.informacao == 'esquerda':
+        elif self.informacao == String('esquerda'):
+            rospy.loginfo('!!!!!')
             self.controle.esquerda_light()
 
-        elif self.informacao == 'muito direita':
+        elif self.informacao == String('muito direita'):
+            rospy.loginfo('!!!!!')
             self.controle.direita_hard()
 
-        elif self.informacao == 'muito esquerda':
+        elif self.informacao == String('muito esquerda'):
+            rospy.loginfo('!!!!!')
             self.controle.esquerda_hard()
             
         return 'repete'
@@ -78,47 +79,54 @@ class  Andarfinal(smach.State):
         self.sub = rospy.Subscriber('/hrdeparar', String, self.callback)
         self.sub1 = rospy.Subscriber('/andar2', String, self.callback1)
         self.parou = False
-        self.informacao1 = None
+        self.informacao = None
 
     def callback(self,data):
         print (data)
-        if data == 'anda um pouco e para':
+        if data == String('anda um pouco e para'):
             self.parou = True
     
     def callback1(self,data):
-        self.informacao1 = data
+        self.informacao = data
 
     def execute(self, ud):
+        rospy.sleep(2)
         if self.parou:
             return 'foi'
 
         if self.informacao is None:
+            rospy.loginfo('??????')
             return 'repete'
 
-        if self.informacao == 'frente':
+        if self.informacao == String('frente'):
+            rospy.loginfo('!!!!!')
             self.controle.pra_frente()
 
-        elif self.informacao == 'direita':
+        elif self.informacao == String('direita'):
+            rospy.loginfo('!!!!!')
             self.controle.direita_light()
 
-        elif self.informacao == 'esquerda':
+        elif self.informacao == String('esquerda'):
+            rospy.loginfo('!!!!!')
             self.controle.esquerda_light()
 
-        elif self.informacao == 'muito direita':
+        elif self.informacao == String('muito direita'):
+            rospy.loginfo('!!!!!')
             self.controle.direita_hard()
 
-        elif self.informacao == 'muito esquerda':
+        elif self.informacao == String('muito esquerda'):
+            rospy.loginfo('!!!!!')
             self.controle.esquerda_hard()
             
         return 'repete'
 
-sm = smach.StateMachine(outcomes=['Deu'])
+sm = smach.StateMachine(outcomes=['Acabou'])
 
 with sm:
     smach.StateMachine.add('StandBy', Sinal(), transitions={'foi':'SeguindoLinhaObj','repete':'StandBy'})
     smach.StateMachine.add('SeguindoLinhaObj', Andarobj(), transitions={'foi':'SeguindoLinhaFinal','repete':'SeguindoLinhaObj'})
     #smach.StateMachine.add('IndoAteObj', Andar2(), transitions={'foi':'ReProcurandoLinha','repete':'IndoAteObj'})
     #smach.StateMachine.add('ReProcurandoLinha', Virar(), transitions={'foi':'SeguindoLinhaFinal','repete':'ReProcurandoLinha'})
-    smach.StateMachine.add('SeguindoLinhaFinal', Andarfinal(), transitions={'foi':'StandBy','repete':'SeguindoLinhaFinal'})
+    smach.StateMachine.add('SeguindoLinhaFinal', Andarfinal(), transitions={'foi':'Acabou','repete':'SeguindoLinhaFinal'})
 
 sm.execute()
